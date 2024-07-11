adf5d940ad3217f0dfc75b673d3087cb511e9cda
palaziv <ivo.palazzolo@gmx.de>
2024-07-11 20:12:34 +0000

    added ability to add multiple root domains

--- stage1.sh
+++ stage1.sh
@@ -2,17 +2,19 @@
 
 baseDir="$(pwd)/recon"
 
-# Prompt user for domain input
-read -p "Enter root domain: " root_domain
-
 read -p "Enter organization name: " org_name
+
+read -p "Enter root domains (separated by spaces): " root_domains
 
 if [ ! -d "${baseDir}/${org_name}" ]; then
         mkdir ${baseDir}/${org_name}
         echo "Created directory: '$org_name'"
-        echo ${root_domain} > "${baseDir}/${org_name}/rootdomain.txt"
+        for domain in ${root_domains}; do
+            echo ${domain} >> "${baseDir}/${org_name}/rootdomain.txt"
+        done
 else
         echo "'$org_name' already exists."
         echo "Finding subdomains..."
 fi
+
 ./stage2.sh "${baseDir}/${org_name}" $org_name
