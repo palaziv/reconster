af195a325afa88f851ee9de50a64e5c378386121
palaziv <ivo.palazzolo@gmx.de>
2024-07-15 10:50:17 +0000

    refactoring + replaced mongodb with bbrf

--- insert.sh
+++ insert.sh
@@ -4,19 +4,37 @@ dir=$1
 programName=$(basename "$dir")
 
 # check if bbrf program already exists
-output=$(bbrf programs --show-empty-programs)
+output=$(bbrf programs --show-empty-scope)
 if echo "$output" | grep -qw "$programName"; then
     echo "BBRF: program $programName found"
 else
     echo "BBRF: program not found. Creating one using 'bbrf new $programName'..."
     bbrf new $programName
+
+    read -p "Enter inscope domains/IPs (separated by spaces; note that this is required): " inscope
+    if [ -z "$inscope" ]; then
+        bbrf inscope add $inscope -p $programName
+    else
+        echo "No inscope specified. Exiting ..."
+        exit 1
+    fi
+
+    read -p "Enter outscope domains/IPs (separated by spaces): " outscope
+    if [ -z "$outscope" ]; then
+        bbrf outscope add $outscope -p $programName
+    fi
 fi
 
+echo "BBRF: adding subdomains..."
 # first add all found subdomains even unresolved ones
-cat "$dir/all_subs.txt" | bbrf domain add - -p $programName -s recon
+cat "$dir/all_subs.txt" | bbrf domain add - -p $programName -s subfinder
 
-file_name="$dir/resolved_with_ips.txt"
+# now add/update resolved domains with their ips
+cat "$dir/resolved_with_ips.txt" | bbrf domain add - -p $programName -s dnsx
+# add does not work when the domain already exists, so do an update
+cat "$dir/resolved_with_ips.txt" | bbrf domain update - -p $programName -s dnsx
 
-while read -r line; do
-    bbrf domain add $line -p $programName -s recon
-done < $file_name
+echo "BBRF: adding URLs..."
+# add urls with status code and content length
+cat "$dir/metadata.txt" | awk '{print $1" "$2" "$4}' | tr -d '[]' | bbrf url add - -p $programName -s httpx
+
--- stage2.sh
+++ stage2.sh
@@ -4,7 +4,7 @@ recon() {
         dir=$1
         programName=$2
 
-        echo "Gathering subs for $programName..."
+        echo "Gathering subdomains for $programName..."
         subfinder -dL "${dir}/rootdomain.txt" -all -silent | anew -q "${dir}/all_subs.txt"
 
         echo "Resolving found subdomains..."
@@ -16,35 +16,17 @@ recon() {
 
         echo "Gathering http metadata..."
         # probe both http and https with "-no-fallback"
-        # TODO: check if we can use -q with anew or if the output is necessary
-        httpx -silent -l "${dir}/resolved.txt" -no-fallback -ports 80,443,4000,4443,4080,8000,8080,8443,8888,9090 -sc -title -ct -location -server -td -method -ip -cname -cdn -nc | anew -q "${dir}/metadata.txt"
+        httpx -silent -l "${dir}/resolved.txt" -no-fallback -ports 80,443,4000,4443,4080,8000,8080,8443,8888,9090 -sc -cl -title -ct -location -server -td -nc | anew -q "${dir}/metadata.txt"
 
-        echo "Separating subs by status code..."
-        grep '\[200\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/200.txt"
-        grep '\[301\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/301.txt"
-        grep '\[302\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/302.txt"
-        grep '\[401\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/401.txt"
-        grep '\[403\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/403.txt"
-        grep '\[404\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/404.txt"
-        grep '\[500\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/500.txt"
-        grep '\[502\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/502.txt"
-        grep '\[503\]' "${dir}/metadata.txt" | cut -d " " -f 1 | cut -d "/" -f 3 > "${dir}/503.txt"
-
-        echo "Inserting records into the database..."
-        programName=$(basename "$dir")
-        python3 "./insert.py" "${dir}/all_subs.txt" ${programName}
-
-        echo "Updating status codes..."
-        python3 "./update.py" "${dir}" ${programName} 
+        echo "Adding to BBRF..."
+        ./insert.sh $dir
 }
 
-# Checking if the correct number of arguments are provided
 if [ "$#" -ne 2 ]; then
         echo "Usage: $0 /path/to/directory program/organization name"
         exit 1
 fi
 
-# Extract provided arguments
 dir=$1
 programName=$2
 
